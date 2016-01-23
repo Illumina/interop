7ce3093161dda550ebdfdb0fa8e60302eff04d56
Robert Langlois <ezralanglois@gmail.com>
2016-01-23 08:33:30 -0800

    Updated curl to ssl curl

--- tools/travis-linux-install.sh
+++ tools/travis-linux-install.sh
@@ -9,6 +9,8 @@ sudo add-apt-repository -y ppa:teward/swig3.0
 sudo add-apt-repository -y ppa:beineri/opt-qt551-trusty
 sudo apt-get update
 sudo apt-get install -qq -y libicu-dev swig3.0 doxygen graphviz
+sudo apt-get remove -y curl
+sudo apt-get install -y curl-ssl
 
 
 
