ba18304a94cf58d12b03939a4ab80e53a3c32ad8
Robert Langlois <ezralanglois@gmail.com>
2016-01-23 09:04:17 -0800

    Added ssl library

--- tools/travis-linux-install.sh
+++ tools/travis-linux-install.sh
@@ -9,6 +9,7 @@ sudo add-apt-repository -y ppa:teward/swig3.0
 sudo add-apt-repository -y ppa:beineri/opt-qt551-trusty
 sudo apt-get update
 sudo apt-get install -qq -y libicu-dev swig3.0 doxygen graphviz
+sudo apt-get install -y libssl-dev
 sudo apt-get remove -y curl
 sudo apt-get install -y curl-ssl
 
