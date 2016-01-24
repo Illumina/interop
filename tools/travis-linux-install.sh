3f3f1140d09384b1eea2e3f3293258a88101f8fc
Robert Langlois <ezralanglois@gmail.com>
2016-01-23 21:27:57 -0800

    Try nuget again

--- tools/travis-linux-install.sh
+++ tools/travis-linux-install.sh
@@ -3,7 +3,7 @@
 lsb_release -a
 
 sudo apt-get -qq update
-sudo apt-get install -qq -y mono-devel mono-gmcs nunit-console
+sudo apt-get install -qq -y mono-complete
 
 sudo apt-get install -y libgtest-dev
 sudo add-apt-repository -y ppa:teward/swig3.0
@@ -11,6 +11,7 @@ sudo add-apt-repository -y ppa:beineri/opt-qt551-trusty
 sudo apt-get update
 sudo apt-get install -qq -y libicu-dev swig3.0 doxygen graphviz
 
+nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
 
 
 
