57c68c594151bd82f24a72ab65a7b8f1fd192618
Robert Langlois <rlanglois@illumina.com>
2016-02-05 09:41:57 -0800

    Fix for random failures in building and deployment

--- tools/travis-linux-install.sh
+++ tools/travis-linux-install.sh
@@ -12,10 +12,15 @@ sudo apt-get update -qq
 sudo apt-get install -qq libicu-dev swig3.0 doxygen graphviz
 
 #nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory testrunner
-wget https://github.com/nunit/nunitv2/releases/download/2.6.4/NUnit-2.6.4.zip
-unzip NUnit-2.6.4.zip
+if [ ! -e $HOME/NUnit-2.6.4 ]; then
+    wget https://github.com/nunit/nunitv2/releases/download/2.6.4/NUnit-2.6.4.zip
+    unzip NUnit-2.6.4.zip
+fi
 
-
+if [ ! -e $HOME/rapidxml-1.13 ]; then
+    wget http://downloads.sourceforge.net/project/rapidxml/rapidxml/rapidxml%201.13/rapidxml-1.13.zip
+    unzip rapidxml-1.13.zip
+fi
 
 
 
--- tools/travis-osx-install.sh
+++ tools/travis-osx-install.sh
@@ -13,3 +13,8 @@ brew install https://s3.amazonaws.com/travisbuilds.swig.org/mono.rb
 # add swig
 # add nunit
 
+if [ ! -e $HOME/rapidxml-1.13 ]; then
+    wget http://downloads.sourceforge.net/project/rapidxml/rapidxml/rapidxml%201.13/rapidxml-1.13.zip
+    unzip rapidxml-1.13.zip
+fi
+
