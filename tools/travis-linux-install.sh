ed56639e8705acb2ff7a2ea96d55f49715aff4e7
Robert Langlois <rlanglois@illumina.com>
2016-02-05 12:26:47 -0800

    Debug travis ci build

--- tools/travis-linux-install.sh
+++ tools/travis-linux-install.sh
@@ -16,7 +16,8 @@ if [ ! -e $HOME/NUnit-2.6.4 ]; then
     wget https://github.com/nunit/nunitv2/releases/download/2.6.4/NUnit-2.6.4.zip
     unzip NUnit-2.6.4.zip
 fi
-
+echo `pwd`
+echo "$HOME"
 
 
 
