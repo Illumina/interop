8fd7c46a07a62e71a0a970a9ac851f9fa5dc3712
Robert Langlois <rlanglois@illumina.com>
2016-09-09 12:45:06 -0700

    Fix double free crash for debug mode in MSVC

--- cmake/InternalUtils.cmake
+++ cmake/InternalUtils.cmake
@@ -18,7 +18,7 @@ macro(fix_default_compiler_settings_)
         foreach (flag_var
                 CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
                 CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
-            if (NOT BUILD_SHARED_LIBS AND NOT gtest_force_shared_crt)
+            if (NOT BUILD_SHARED_LIBS AND NOT FORCE_SHARED_CRT)
                 # When Google Test is built as a shared library, it should also use
                 # shared runtime libraries.  Otherwise, it may end up with multiple
                 # copies of runtime library data in different modules, resulting in
