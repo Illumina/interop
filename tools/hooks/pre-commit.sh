d657a28509d05078c81124f6c36933947949fa66
Robert Langlois <rlanglois@illumina.com>
2016-09-23 16:55:36 -0700

    Remove unused code

--- tools/hooks/pre-commit.sh
+++ tools/hooks/pre-commit.sh
@@ -9,14 +9,9 @@ if [[ "$BRANCH" == "master" ]]; then
 	exit 1
 fi
 
-git diff --cached --exit-code --quiet
-status1=$?
-git diff --exit-code --quiet
-status2=$?
-if [ "$status1" == "1" ] || ["$status2" == "1"] ; then
-version=`git describe --tags --dirty=-src`
-cp CMakeLists.txt .CMakeLists.txt.bak
-sed "s/set(ARCHIVE_VERSION .*)/set(ARCHIVE_VERSION \"${version}\")/g" .CMakeLists.txt.bak > CMakeLists.txt
-git add CMakeLists.txt
-git commit --amend -C HEAD --no-verify
-fi
+#git diff --cached --exit-code --quiet
+#status1=$?
+#git diff --exit-code --quiet
+#status2=$?
+#if [ "$status1" == "1" ] || ["$status2" == "1"] ; then
+#fi
