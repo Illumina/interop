eb4195173907ca75a6cf54e6ffc80b7d9c7b88f6
Robert Langlois <rlanglois@illumina.com>
2020-12-03 06:32:01 -0800

    Reduce reqs

--- tools/prereqs/travis-osx-install.sh
+++ tools/prereqs/travis-osx-install.sh
@@ -4,25 +4,21 @@
 
 #brew update > /dev/null
 #brew list
-which cmake || brew unlink cmake
-time brew install cmake
-brew link cmake
+brew list cmake > /dev/null || time brew upgrade cmake
 brew install zlib
 time brew install swig@3
 brew unlink swig || true
 brew link swig@3 --force
 time brew install doxygen
 brew install wget
-#brew install mono
-brew remove mono || true
 brew install nuget
 brew install coreutils || brew install gstat
-#brew install openssl@1.1
-brew uninstall openssl && brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/8b9d6d688f483a0f33fcfc93d433de501b9c3513/Formula/openssl.rb
-brew outdated pyenv || brew upgrade pyenv
-brew cask list dotnet-sdk > /dev/null || time brew cask install dotnet-sdk
 
-curl -o miniconda.sh  https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+brew tap isen-ng/dotnet-sdk-versions
+brew cask list dotnet-sdk > /dev/null || time brew cask install dotnet-sdk2-2-400
+#brew cask list dotnet-sdk > /dev/null || time brew cask install dotnet-sdk
+
+time curl -o miniconda.sh  https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
 time bash miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/etc/profile.d/conda.sh
 
