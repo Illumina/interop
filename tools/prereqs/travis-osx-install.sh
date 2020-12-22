0f735711401a83a9ffe029375973571f9e81d9a8
Robert Langlois <rlanglois@illumina.com>
2020-12-21 19:42:03 -0800

    Clean up

--- tools/prereqs/travis-osx-install.sh
+++ tools/prereqs/travis-osx-install.sh
@@ -2,25 +2,18 @@
 
 echo "Install requirements using HomeBrew"
 export HOMEBREW_NO_INSTALL_CLEANUP=1
-export HOMEBREW_NO_AUTO_UPDATE=1
-#sw_vers
+export HOMEBREW_NO_AUTO_UPDATE=1 # Fix
 
 set -x
 
-#brew update #> /dev/null
-#brew list
-#brew list cmake > /dev/null || time brew upgrade cmake
-#brew list zlib || brew install zlib
 brew list swig@3 || brew install swig@3
 brew unlink swig || true
 brew link swig@3 --force
 brew install doxygen
-#brew install wget
 brew install nuget
-#brew install coreutils || brew install gstat
 
 brew tap isen-ng/dotnet-sdk-versions
-brew cask list dotnet-sdk > /dev/null || time brew cask install dotnet-sdk2-2-400
+brew cask list dotnet-sdk || brew cask install dotnet-sdk2-2-400
 
 brew list miniconda
 
@@ -31,11 +24,8 @@ time curl -o miniconda.sh  https://repo.anaconda.com/miniconda/Miniconda3-latest
 time bash miniconda.sh -b -p $HOME/miniconda
 source $HOME/miniconda/etc/profile.d/conda.sh
 
-#echo "OpenSSL: $(brew --prefix openssl)"
-#export PATH=$(pyenv root)/shims:${PATH}
-
 which conda
-#dotnet --version
+dotnet --version
 
 set +x
 echo "Finished requirements"
