a3d5e86db430f7720d15055cdbc752573e3abfae
rlanglois <rlanglois@illumina.com>
2024-07-12 20:32:54 -0700

    Try once more

--- tools/package.sh
+++ tools/package.sh
@@ -255,8 +255,9 @@ elif [ "$PYTHON_VERSION" != "" ] && [ "$PYTHON_VERSION" != "Disable" ] && [ "$PY
         pip install swig --prefix ./usr
         swig_bin=$(ls ./usr/lib/python3.*/site-packages/swig/data/bin/swig)
         swig_path=$(dirname $swig_bin)
+        swig_dir=$(dirname $swig_path)
         export PATH=${swig_path}:$PATH
-        export SWIG_DIR=${swig_path}/..
+        export SWIG_DIR=${swig_dir}/share
         run "Configure $py_ver" cmake $SOURCE_PATH -B${BUILD_PATH} ${CMAKE_EXTRA_FLAGS} -DENABLE_CSHARP=OFF -DENABLE_PYTHON_DYNAMIC_LOAD=ON -DPython_EXECUTABLE=`which python` -DSKIP_PACKAGE_ALL_WHEEL=ON -DPYTHON_WHEEL_PREFIX=${ARTIFACT_PATH}/tmp
         run "Build $py_ver" cmake --build $BUILD_PATH -- -j${THREAD_COUNT}
         run "Test $py_ver" cmake --build $BUILD_PATH --target check_python -- -j${THREAD_COUNT}
