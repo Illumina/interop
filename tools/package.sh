cc04d3477932f91db73c8ee83885c7f5fbb1ca58
rlanglois <rlanglois@illumina.com>
2025-12-12 11:30:44 -0800

    Address more comments

--- tools/package.sh
+++ tools/package.sh
@@ -31,7 +31,7 @@ whoami 1>/dev/null 2>&1 || export HOME=$PWD
 
 # Get value from environment for low memory vms
 if [ -z $THREAD_COUNT ] ; then
-    THREAD_COUNT=1
+    THREAD_COUNT=4
 fi
 
 if [ ! -z $1 ] ; then
